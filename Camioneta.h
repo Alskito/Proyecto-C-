74991e43471d568b5802beccf0bec242ac7c7a33
Alskito <zamudioalexander2@gmail.com>
2025-11-20 15:31:12 -0600

    Camioneta.h
    Se añadió el atributo capacidad_carga y sus métodos set y get

--- Camioneta.h
+++ Camioneta.h
@@ -8,22 +8,25 @@ class Camioneta {
     int precio_original;
     int year;
     int kilometraje;
+    float capacidad_carga;
 
     public:
-    Camioneta (): marca(""), modelo(""), precio_original(0), year(0), kilometraje(0) {};
-    Camioneta (std::string mar, std::string mod, int pre, int ye, int kil): marca(mar), modelo(mod), precio_original(pre), year(ye), kilometraje(kil){};
+    Camioneta (): marca(""), modelo(""), precio_original(0), year(0), kilometraje(0), capacidad_carga(0.0) {};
+    Camioneta (std::string mar, std::string mod, int pre, int ye, int kil, float cap): marca(mar), modelo(mod), precio_original(pre), year(ye), kilometraje(kil), capacidad_carga(cap) {};
 
     std::string get_marca();
     std::string get_modelo();
     int get_precio_original();
     int get_year();
     int get_kilometraje();
+    float get_capacidad_carga(); // En litros
 
     void set_marca(std::string ); 
     void set_modelo(std::string ); 
     void set_precio_original(int );
     void set_year(int );
     void set_kilometraje(int );
+    void set_capacidad_carga(float );
 };
 
 std::string Camioneta ::get_marca(){
@@ -46,6 +49,10 @@ int Camioneta ::get_kilometraje(){
     return kilometraje;
 }
 
+float Camioneta ::get_capacidad_carga(){
+    return capacidad_carga;
+}
+
 void Camioneta ::set_marca(std::string mar){
     marca = mar;
 }
@@ -64,4 +71,8 @@ void Camioneta ::set_year(int ye){
 
 void Camioneta ::set_kilometraje(int kil){
     kilometraje = kil;
+}
+
+void Camioneta ::set_capacidad_carga(float cap){
+    capacidad_carga = cap;
 }
