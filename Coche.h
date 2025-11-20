b1c81e8e971d29eda7e6187e155d8b27b7762d90
Alskito <zamudioalexander2@gmail.com>
2025-11-20 15:32:55 -0600

    Coche.h
    Se añadió el atributo puertas, juntos con sus métodos get y set

--- Coche.h
+++ Coche.h
@@ -8,22 +8,25 @@ class Coche{
     int precio_original;
     int year;
     int kilometraje;
+    int puertas;
 
     public:
-    Coche(): marca(""), modelo(""), precio_original(0), year(0), kilometraje(0) {};
-    Coche(std::string mar, std::string mod, int pre, int ye, int kil): marca(mar), modelo(mod), precio_original(pre), year(ye), kilometraje(kil){};
+    Coche(): marca(""), modelo(""), precio_original(0), year(0), kilometraje(0), puertas(0) {};
+    Coche(std::string mar, std::string mod, int pre, int ye, int kil, int pu): marca(mar), modelo(mod), precio_original(pre), year(ye), kilometraje(kil), puertas(pu){};
 
     std::string get_marca();
     std::string get_modelo();
     int get_precio_original();
     int get_year();
     int get_kilometraje();
+    int get_puertas();
 
     void set_marca(std::string ); 
     void set_modelo(std::string ); 
     void set_precio_original(int );
     void set_year(int );
     void set_kilometraje(int );
+    void set_puertas(int );
 };
 
 std::string Coche::get_marca(){
@@ -46,6 +49,10 @@ int Coche::get_kilometraje(){
     return kilometraje;
 }
 
+int Coche::get_puertas(){
+    return puertas;
+}
+
 void Coche::set_marca(std::string mar){
     marca = mar;
 }
@@ -66,3 +73,6 @@ void Coche::set_kilometraje(int kil){
     kilometraje = kil;
 }
 
+void Coche::set_puertas(int pu){
+    puertas = pu;
+}
