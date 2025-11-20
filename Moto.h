241d492f6c953cb80ba93b53b59d5aa8a932353a
Alskito <zamudioalexander2@gmail.com>
2025-11-20 15:35:22 -0600

    Moto.h
    Se añadió el atributo cc, junto con sus métodos set y get

--- Moto.h
+++ Moto.h
@@ -8,22 +8,25 @@ class Moto{
     int precio_original;
     int year;
     int kilometraje;
+    int cc; //indica la potencia y el rendimiento de la moto
 
     public:
-    Moto(): marca(""), modelo(""), precio_original(0), year(0), kilometraje(0) {};
-    Moto(std::string mar, std::string mod, int pre, int ye, int kil): marca(mar), modelo(mod), precio_original(pre), year(ye), kilometraje(kil){};
+    Moto(): marca(""), modelo(""), precio_original(0), year(0), kilometraje(0), cc(0) {};
+    Moto(std::string mar, std::string mod, int pre, int ye, int kil, int c): marca(mar), modelo(mod), precio_original(pre), year(ye), kilometraje(kil), cc(c){};
 
     std::string get_marca();
     std::string get_modelo();
     int get_precio_original();
     int get_year();
     int get_kilometraje();
+    int get_cc();
 
     void set_marca(std::string ); 
     void set_modelo(std::string ); 
     void set_precio_original(int );
     void set_year(int );
     void set_kilometraje(int );
+    void set_cc(int );
 };
 
 std::string Moto::get_marca(){
@@ -46,6 +49,10 @@ int Moto::get_kilometraje(){
     return kilometraje;
 }
 
+int Moto::get_cc(){
+    return cc;
+}
+
 void Moto::set_marca(std::string mar){
     marca = mar;
 }
@@ -64,4 +71,8 @@ void Moto::set_year(int ye){
 
 void Moto::set_kilometraje(int kil){
     kilometraje = kil;
+}
+
+void Moto::set_cc(int c){
+    cc = c;
 }
